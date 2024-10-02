fad18a848275aac46c39bbd9e97a6269632742af
arsnazarenko <arsnazarenko@gmail.com>
2024-10-02 16:56:02 +0300

    Add inverted index and search iterator implementations

---
+++ index/index.go
@@ -0,0 +1,172 @@
+package index
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Постинг лист - список документов, содержащих данный терм
+// Индекс в памяти
+type InvertedIndex struct {
+	docs  []Document
+	index map[string]PostingList
+}
+
+type Document struct {
+	DID  DocumentID
+	Text string
+}
+
+func NewInMemoryIndex() (*InvertedIndex, error) {
+	return &InvertedIndex{
+		docs:  []Document{},
+		index: make(map[string]PostingList),
+	}, nil
+}
+
+// Строим индекс в памяти
+func NewInMemoryIndexFrom(docs []Document) (*InvertedIndex, error) {
+	if len(docs) == 0 {
+		return nil, fmt.Errorf("docs len in 0")
+	}
+
+	inmem, err := NewInMemoryIndex()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, d := range docs {
+		inmem.AddDocument(d)
+	}
+	return inmem, nil
+}
+
+// Нормализация текста
+
+func (in *InvertedIndex) Search(term string) *Iterator {
+	if v, ok := in.index[term]; ok {
+		return &Iterator{
+			index: in,
+			pl:    v,
+			pos:   0,
+		}
+	}
+	return &Iterator{
+		index: in,
+		pl:    PostingList{},
+		pos:   0,
+	}
+}
+
+func (in *InvertedIndex) AddDocument(doc Document) error {
+	normalizedText := normalizeText(doc.Text)
+	in.docs = append(in.docs, doc)
+	// Разбиение текста на токены
+	tokens := strings.Fields(normalizedText)
+	for _, token := range tokens {
+		// Добавление документа в постинг лист
+		if _, ok := in.index[token]; ok {
+			in.index[token] = append(in.index[token], doc.DID)
+		} else {
+			in.index[token] = PostingList{doc.DID}
+		}
+	}
+	return nil
+}
+
+// |PAYLOAD|META|META_OFFSET(2B)|
+//
+// |len|token|posting_list|len|token|posting_list|len|token|posting_list|
+
+func (in *InvertedIndex) Save(path string) error {
+	file, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	b := bytes.NewBuffer(make([]byte, 0, 4096))
+	offsets := []uint16{}
+	var tmp [binary.MaxVarintLen64]byte
+
+	for token, pl := range in.index {
+		offsets = append(offsets, uint16(b.Len()))
+		n := binary.PutUvarint(tmp[:], uint64(len(token)))
+		raw := pl.Encode()
+		b.Write(tmp[:n])
+		b.WriteString(token)
+		b.Write(raw)
+	}
+	metaOffset := b.Len()
+	for i := 1; i < len(offsets); i++ {
+		binary.LittleEndian.PutUint16(tmp[:], offsets[i])
+		b.Write(tmp[:2])
+	}
+	binary.LittleEndian.PutUint16(tmp[:], uint16(metaOffset))
+	b.Write(tmp[:2])
+
+	n, err := file.Write(b.Bytes())
+	if err != nil || n != b.Len() {
+		return fmt.Errorf("Write error: %w", err)
+	}
+	file.Sync()
+	return nil
+}
+
+// |len|str|pl len|str|pl len|str|pl|...|2, 4|5|
+// |          |          |          |
+func LoadFromFile(path string, docs []Document) (*InvertedIndex, error) {
+	var (
+		res map[string]PostingList = make(map[string]PostingList)
+	)
+
+	raw, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	metaOffset := binary.LittleEndian.Uint16(raw[len(raw)-2:])
+
+	payload := raw[:metaOffset]
+	meta := raw[metaOffset : len(raw)-2]
+
+	offsets := []int{0}
+	for i := 0; i < len(meta); i += 2 {
+
+		off := binary.LittleEndian.Uint16(meta[i : i+2])
+		offsets = append(offsets, int(off))
+	}
+	offsets = append(offsets, int(metaOffset))
+	for i := 0; i < len(offsets)-1; i++ {
+		start, end := offsets[i], offsets[i+1]
+		view := payload[start:end]
+		idx := 0
+		tokenLen, n := binary.Uvarint(view[idx:])
+		idx += n
+
+		token := string(view[idx : idx+int(tokenLen)])
+		idx += int(tokenLen)
+		var pl PostingList
+		pl.Decode(view[idx:])
+		res[token] = pl
+	}
+	return &InvertedIndex{
+		docs:  docs,
+		index: res,
+	}, nil
+}
+
+func normalizeText(text string) string {
+	// Удаляем знаки препинания и приводим к нижнему регистру
+	r := strings.NewReplacer(".", "", ",", "", "!", "", "?", "", ":", "")
+	return strings.ToLower(r.Replace(text))
+}
+
+func (in *InvertedIndex) String() string {
+	b := strings.Builder{}
+	for term, postingList := range in.index {
+		b.WriteString(fmt.Sprintf("%s: %v\n", term, postingList))
+	}
+	return b.String()
+}
---
+++ index/iterator.go
@@ -0,0 +1,120 @@
+package index
+
+import (
+	"cmp"
+	"slices"
+)
+
+type Iterator struct {
+	index *InvertedIndex
+	pl    PostingList
+	pos   int
+}
+
+func (it *Iterator) HasNext() bool {
+	return it.pos < len(it.pl)
+}
+
+func (it *Iterator) Next() {
+	it.pos++
+}
+
+func (it *Iterator) Get() DocumentID {
+	return it.pl[it.pos]
+}
+
+func Or(it1, it2 *Iterator) *Iterator {
+	res := mergeSlices(it1.pl, it2.pl)
+
+	return &Iterator{
+		index: it1.index,
+		pl:    res,
+		pos:   0,
+	}
+}
+
+func And(it1, it2 *Iterator) *Iterator {
+    res := intersectSlices(it1.pl, it2.pl)
+	return &Iterator{
+		index: it1.index,
+		pl:    res,
+		pos:   0,
+	}
+
+}
+
+func Not(it *Iterator) *Iterator {
+    fullDocsPl := make(PostingList, 0, len(it.index.docs))
+    for _, d := range it.index.docs {
+        fullDocsPl = append(fullDocsPl, d.DID)
+    }
+    res := subtractSlices(it.pl, fullDocsPl)
+	return &Iterator{
+		index: it.index,
+		pl:    res,
+		pos:   0,
+	}
+}
+
+func mergeSlices[T cmp.Ordered](s1, s2 []T) []T {
+	// Создаем карту для отслеживания уже добавленных элементов
+	seen := make(map[T]bool)
+	// Создаем новый слайс для результата
+	// Добавляем все элементы из всех слайсов в карту и результат
+	for _, v := range s1 {
+		seen[v] = true
+	}
+
+	for _, v := range s2 {
+		if !seen[v] {
+			s1 = append(s1, v)
+			seen[v] = true
+		}
+	}
+	// Сортируем результирующий слайс
+	slices.Sort(s1)
+	return s1
+}
+
+func intersectSlices[T cmp.Ordered](slice1, slice2 []T) []T {
+	// Создаем карту для отслеживания элементов из первого слайса
+	seen := make(map[T]bool)
+	for _, v := range slice1 {
+		seen[v] = true
+	}
+	// Создаем новый слайс для результата
+	result := make([]T, 0)
+	// Итерируем по второму слайсу, добавляя элементы в результат, если они присутствуют в карте
+	for _, v := range slice2 {
+		if seen[v] {
+			result = append(result, v)
+		}
+	}
+	slices.Sort(result)
+	// Сортируем результирующий слайс
+	return result
+}
+
+func subtractSlices[T cmp.Ordered](slice1, slice2 []T) []T {
+	// Создаем карту для отслеживания элементов из второго слайса
+	seen := make(map[T]bool)
+	var small, big []T = slice1, slice2
+	if len(slice1) > len(slice2) {
+		small = slice2
+		big = slice1
+
+	}
+	for _, v := range small {
+		seen[v] = true
+	}
+	// Создаем новый слайс для результата
+	result := make([]T, 0)
+	// Итерируем по первому слайсу, добавляя элементы в результат, если они не присутствуют в карте
+	for _, v := range big {
+		if !seen[v] {
+			result = append(result, v)
+		}
+	}
+	slices.Sort(result)
+	return result
+}
